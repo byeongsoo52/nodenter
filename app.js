6bd477828256f88127924f40e4eafb6e0df7f125
byeongsoo52 <132368027+byeongsoo52@users.noreply.github.com>
2023-06-16 11:11:10 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+// express 를 가져와서 서버를 실행 
+
+const express = require('express')
+const app = express();
+const port = 3000;
+
+// const commentsRouter = require("./routes/comments");
+// // connect라는 변수에 require로 schemas 모듈을 가지고온다
+// const connect = require("./schemas/");
+// connect(); 
+
+const postsRouter = require("./routes/posts");
+const connect = require("./schemas/");
+connect(); 
+
+
+app.use(express.json());
+// app.use("/api", [commentsRouter])
+app.use("/api", [postsRouter])
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(port, () => {
+    console.log(port, '포트로 서버가 열렸어요!')
+})
